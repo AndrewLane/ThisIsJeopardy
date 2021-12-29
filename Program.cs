a755c2a2f1ab48139e549d92dc65f70db7f405c4
Andrew Lane <andrew.lane@powerschool.com>
2021-12-28 23:02:29 -0500

    Because I can't figure out how a local dev environment could work, inject an ApiHelperService that will use an alternate localhost API URL when appropriate

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ThisIsJeopardy;
+using ThisIsJeopardy.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ApiHelperService>();
 
 await builder.Build().RunAsync();
