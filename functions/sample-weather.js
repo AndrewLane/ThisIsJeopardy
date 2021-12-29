0028c4a42619491055265503a40a4c3d548da49c
Andrew Lane <andrew.lane@powerschool.com>
2021-12-29 08:55:25 -0500

    Remove some debug info

--- functions/sample-weather.js
+++ functions/sample-weather.js
@@ -5,9 +5,7 @@ export async function onRequestGet(context) {
     {
       date: "2021-10-31",
       temperatureC: 20,
-      summary: `Balmy at ${request.headers.get(
-        "CF-Connecting-IP"
-      )} and miniflare check is ${globalThis.MINIFLARE ? "on" : "off"}`,
+      summary: `Balmy at ${request.headers.get("CF-Connecting-IP")}`,
     },
     {
       date: "2021-12-25",
