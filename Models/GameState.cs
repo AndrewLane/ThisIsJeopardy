0bb0210513f7526d9007cc098af366b3a01d5191
Andrew Lane <andrew.lane@powerschool.com>
2021-12-31 12:18:39 -0500

    Add pages for both hosting a game and game play * Initial implementation * Add infrastructure for refreshing the game state on a timer

---
+++ Models/GameState.cs
@@ -0,0 +1,10 @@
+﻿namespace ThisIsJeopardy.Models;
+
+public class GameState
+{
+    public string? GameId { get; set; }
+    public string? PlayerId1 { get; set; }
+    public string? PlayerId2 { get; set; }
+    public DateTime? BuzzedInTime { get; set; }
+    public string? BuzzedInPlayer { get; set; }
+}
---
+++ Services/TimerService.cs
@@ -0,0 +1,46 @@
+﻿using System.Timers;
+
+namespace ThisIsJeopardy.Services;
+
+/// <summary>
+/// Source: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-6.0&viewFallbackFrom=aspnetcore-3.0#invoke-component-methods-externally-to-update-state
+/// </summary>
+public class TimerService : IDisposable
+{
+    private int elapsedCount;
+    private readonly ILogger<TimerService> logger;
+    private readonly NotifierService notifier;
+    private System.Timers.Timer? timer;
+    private const int tickInterval = 500;
+
+    public TimerService(NotifierService notifier, ILogger<TimerService> logger)
+    {
+        this.notifier = notifier;
+        this.logger = logger;
+    }
+
+    public void Start()
+    {
+        if (timer is null)
+        {
+            timer = new();
+            timer.AutoReset = true;
+            timer.Interval = tickInterval;
+            timer.Elapsed += HandleTimer;
+            timer.Enabled = true;
+            logger.LogInformation("Timer Started");
+        }
+    }
+
+    private async void HandleTimer(object? source, ElapsedEventArgs e)
+    {
+        elapsedCount += 1;
+        await notifier.Update("elapsedCount", elapsedCount);
+        // logger.LogInformation($"elapsedCount: {elapsedCount}");
+    }
+
+    public void Dispose()
+    {
+        timer?.Dispose();
+    }
+}
